a41f9d4bef2c02cba40c9b2293c418bdd8afa902
Ben Klein <bklein@pivotal.io>
2016-01-19 21:32:17 -0600

    Add complete version

---
+++ complete/book/src/main/java/hello/BookApplication.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BookApplication {
+
+  @RequestMapping(value = "/available")
+  public String available() {
+    return "Spring in Action";
+  }
+
+  @RequestMapping(value = "/checked-out")
+  public String checkedOut() {
+    return "Spring Boot in Action";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookApplication.class, args);
+  }
+}
