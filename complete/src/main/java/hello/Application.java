3bbc2110f78938213aa143d483aa5a027869b88b
botros_fadi <botros_fadi@yahoo.com>
2017-01-20 23:54:13 +0200

    Some correction in Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses=mysqlapp.repositories.UsersRepository.class)
+@EnableJpaRepositories(basePackageClasses=hello.repositories.UsersRepository.class)
 // This tells Spring's repository scanner to scan the package which contains the mentioned repository (ALL THE PACKAGE),here you could put the parameter 'considerNestedRepositories' if your repository is inner
-@EntityScan(basePackageClasses=mysqlapp.models.User.class) // This tells Spring to scan ALL
+@EntityScan(basePackageClasses=hello.models.User.class) // This tells Spring to scan ALL
 // THE PACKAGE which CONTAINS the User class entity for entities
 public class Application {
 
