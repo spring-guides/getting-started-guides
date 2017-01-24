8049589be242cabdbc6a80ea40b37ae4719a9060
botros_fadi <botros_fadi@yahoo.com>
2017-01-24 23:33:29 +0200

    Revised changes

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,14 +2,8 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses=hello.repositories.UsersRepository.class)
-// This tells Spring's repository scanner to scan the package which contains the mentioned repository (ALL THE PACKAGE),here you could put the parameter 'considerNestedRepositories' if your repository is inner
-@EntityScan(basePackageClasses=hello.models.User.class) // This tells Spring to scan ALL
-// THE PACKAGE which CONTAINS the User class entity for entities
 public class Application {
 
     public static void main(String[] args) {
--- complete/src/main/java/hello/User.java
+++ complete/src/main/java/hello/User.java
@@ -1,4 +1,4 @@
-package hello.models;
+package hello;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
