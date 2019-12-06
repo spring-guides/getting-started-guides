c4d77ec73e0f12722bbb247cc36822139509ed5c
Mark Paluch <mpaluch@pivotal.io>
2019-12-06 12:08:55 +0100

    Upgrade to Boot 2.2.2 and Boot R2DBC 0.1.0.M3

--- complete/src/main/java/com/example/accessingdatar2dbc/CustomerRepository.java
+++ complete/src/main/java/com/example/accessingdatar2dbc/CustomerRepository.java
@@ -1,7 +1,8 @@
 package com.example.accessingdatar2dbc;
 
-import org.springframework.data.r2dbc.repository.query.Query;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
 import reactor.core.publisher.Flux;
 
 public interface CustomerRepository extends ReactiveCrudRepository<Customer, Long> {
